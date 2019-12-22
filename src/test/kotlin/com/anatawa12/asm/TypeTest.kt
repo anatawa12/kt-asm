52bfb1aa233287f17e17ebe4ee320cebf4980dca
anatawa12 <anatawa12@icloud.com>
2019-12-22 22:57:19 +0900

    add test for Type

---
+++ src/test/kotlin/com/anatawa12/asm/TypeTest.kt
@@ -0,0 +1,64 @@
+package com.anatawa12.asm
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by anatawa12 on 2019/12/22.
+ */
+class TypeTest {
+    @Test
+    fun getKind() {
+        assertEquals(Type.Kind.Class, Type("Ljava/lang/String;").kind)
+        assertEquals(Type.Kind.Array, Type("[Ljava/lang/String;").kind)
+        assertEquals(Type.Kind.Primitive, Type("I").kind)
+        assertEquals(Type.Kind.Primitive, Type("V").kind)
+    }
+
+    @Test
+    fun getInternalName() {
+        assertEquals(InternalName("java/lang/String"), Type("Ljava/lang/String;").internalName)
+        assertEquals(InternalName("[Ljava/lang/String;"), Type("[Ljava/lang/String;").internalName)
+        assertEquals(InternalName("I"), Type("I").internalName)
+    }
+
+    @Test
+    fun getElementType() {
+        assertEquals(Type("Ljava/lang/String;"), Type("[Ljava/lang/String;").elementType)
+        assertEquals(Type("[Ljava/lang/String;"), Type("[[Ljava/lang/String;").elementType)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getElementTypeClass() {
+        Type("Ljava/lang/String;").elementType
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getElementTypePrimitive() {
+        Type("V").elementType
+    }
+
+    @Test
+    fun getDimensions() {
+        assertEquals(0, Type("Ljava/lang/String;").dimensions)
+        assertEquals(1, Type("[Ljava/lang/String;").dimensions)
+        assertEquals(2, Type("[[Ljava/lang/String;").dimensions)
+    }
+
+    @Test
+    fun getBottomElementType() {
+        assertEquals(Type("Ljava/lang/String;"), Type("Ljava/lang/String;").bottomElementType)
+        assertEquals(Type("Ljava/lang/String;"), Type("[Ljava/lang/String;").bottomElementType)
+        assertEquals(Type("Ljava/lang/String;"), Type("[[Ljava/lang/String;").bottomElementType)
+    }
+
+    @Test
+    fun getSize() {
+        assertEquals(0, Type("V").size)
+        assertEquals(1, Type("Ljava/lang/String;").size)
+        assertEquals(1, Type("[Ljava/lang/String;").size)
+        assertEquals(1, Type("I").size)
+        assertEquals(2, Type("J").size)
+        assertEquals(2, Type("D").size)
+    }
+}
