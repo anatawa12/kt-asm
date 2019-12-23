90e17d1c9e195f71393bc3f61a32ccd57cc487da
anatawa12 <anatawa12@icloud.com>
2019-12-23 19:41:27 +0900

    add ModulePipeFromOW2

---
+++ src/main/kotlin/com/anatawa12/asm/internal/ModulePipeFromOW2.kt
@@ -0,0 +1,48 @@
+package com.anatawa12.asm.internal
+
+import com.anatawa12.asm.InternalName
+import com.anatawa12.asm.ModuleVisitor
+import com.anatawa12.asm.Type
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.ModuleVisitor as ModuleVisitorOW2
+
+/**
+ * Created by anatawa12 on 2019/12/23.
+ */
+class ModulePipeFromOW2(
+    val moduleVisitor: ModuleVisitor
+) : ModuleVisitorOW2(Opcodes.ASM7) {
+    override fun visitMainClass(mainClass: String) {
+        moduleVisitor.visitMainClass(Type.objectFromInternalName(mainClass))
+    }
+
+    override fun visitPackage(packageName: InternalName) {
+        moduleVisitor.visitPackage(packageName)
+    }
+
+    override fun visitRequire(module: String, access: Int, version: String?) {
+        moduleVisitor.visitRequire(module, access, version)
+    }
+
+    override fun visitExport(packageName: InternalName, access: Int, vararg modules: String) {
+        moduleVisitor.visitExport(packageName, access, *modules)
+    }
+
+    override fun visitOpen(packageName: InternalName, access: Int, vararg modules: String) {
+        moduleVisitor.visitOpen(packageName, access, *modules)
+    }
+
+    override fun visitUse(service: String) {
+        moduleVisitor.visitUse(Type.objectFromInternalName(service))
+    }
+
+    override fun visitProvide(service: String, vararg providers: String) {
+        moduleVisitor.visitProvide(
+            Type.objectFromInternalName(service),
+            *providers.mapArray { Type.objectFromInternalName(it) })
+    }
+
+    override fun visitEnd() {
+        moduleVisitor.visitEnd()
+    }
+}
